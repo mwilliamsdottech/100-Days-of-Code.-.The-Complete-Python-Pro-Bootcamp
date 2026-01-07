90bd0088c7e0d4a417d04ffc6cffed480390f2cd
mwilliamsdottech <michael@mwilliams.tech>
2026-01-07 07:19:48 -0500

    *

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -13,17 +13,24 @@ print(placeholder)
 # TODO-1: - Use a while loop to let the user guess again.
 guesses_remaining = 5
 while guesses_remaining > 0:
-    guess = input("Guess a letter: ").lower()
+    guess = input("\nGuess a letter: ").lower()
 
+    if guess in chosen_word:
 
-
-
-    # TODO-2: Change the for loop so that you keep the previous correct letters in display.
-    display = ""
-    for letter in chosen_word:
-        if letter == guess:
-            display += letter
+        # TODO-2: Change the for loop so that you keep the previous correct letters in display.
+        display = ""
+        for letter in chosen_word:
+            if letter == guess:
+                display += letter
+            else:
+                display += "_"
+        print(display)
+    else:
+        guesses_remaining -= 1
+        if guesses_remaining > 1:
+            print(f"Try again. {guesses_remaining} guesses remaining.")
         else:
-            display += "_"
+            print(f"Try again. {guesses_remaining} guess remaining.")
 
-    print(display)
+else:
+    print("\nGame Over.")
