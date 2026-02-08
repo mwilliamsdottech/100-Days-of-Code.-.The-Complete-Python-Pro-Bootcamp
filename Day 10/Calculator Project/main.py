e40c51b122ff1aedbc491fff14945143dfbf4712
mwilliamsdottech <michael@mwilliams.tech>
2026-02-08 16:44:15 -0500

    -

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,24 @@
 def add(n1, n2):
     return n1 + n2
 
+def subtract(n1, n2):
+    return n1 - n2
 
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+n1 = input("\nEnter first number: ")
+operation = input("\nEnter + to add \nEnter - to subtract \nEnter * to multiply \nEnter / to divide \n\n:")
+n2 = input("\nEnter second number: ")
+
+if operation
